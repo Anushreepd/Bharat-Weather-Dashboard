2dae142d68d8f2f5d7f096ab0b348bd8e2934b0b
Anushreepd <anushreepd14@gmail.com>
2026-02-05 07:20:39 +0530

    Api integration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import { useState } from "react";
 import StateSelector from "./Component/StateSelector";
-import './App.css';
+import "./App.css";
+import { useWeather } from "./Hooks/useWeather";
 
 function App() {
-const [selectState, setSlectedState] = useState("");
+  const [selectState, setSlectedState] = useState("");
+  const [weather, loading, error] = useWeather(selectState);
 
-const handleChange = (e) => {
-  setSlectedState(e.target.value);
-}
-
+  const handleChange = (e) => {
+    setSlectedState(e.target.value);
+  };
   return (
     <div className="app">
+      <div className="dashboard-container">
       <h1>Bharat Weather Dashboard</h1>
 
-      <StateSelector 
-      selectState  = {selectState}
-      onChange = {handleChange}
+      <StateSelector
+        selectState={selectState}
+        onChange={handleChange}
       ></StateSelector>
-      {selectState && (
+      {loading && <p>Loading weather...</p>}
+      {error && <p>{error}</p>}
+
+      {weather && !loading && (
         <div className="weather-card">
           <h2>{selectState}</h2>
-          <p>Weather details will appear here</p>
+          <p>Capital: {weather.name}</p>
+          <img
+            className="weather-icon"
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt="weather icon"
+          />
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>
+            Condition:{" "}
+            {weather?.weather?.[0]?.description &&
+              weather.weather[0].description[0].toUpperCase() +
+                weather.weather[0].description.slice(1)}
+          </p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Wind: {weather.wind.speed} km/h</p>
         </div>
       )}
+    </div>
     </div>
   );
 }
